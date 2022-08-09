861715569bd22335e4c577997ff9564550b51923
Hamidulloh07 <hsiddiqxojayev2005@gmail.com>
2022-08-09 21:59:33 +0600

    add

--- js/app.js
+++ js/app.js
@@ -2,27 +2,35 @@ let elForm = document.querySelector(".form");
 let elInput = document.querySelector(".form-control");
 let elList = document.querySelector(".list");
 
-
-
 elForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
 
- elInputValue = (elInput.value);
+let elInputValue = (elInput.value);
 
   for (foodElement of elInputValue) {
     foodElement = []
 
     let elItem = document.createElement("li");
-    
+      if (elInputValue <= 0) {
+      alert("son kiritmang")
+      elList.appendChild();
+      console.log();
+      } 
+
+      else if (elInputValue >= 0)  {
+        alert("son kiritmang")
+        elList.appendChild();
+        console.log();
+  
+      }
     foodElement.push(elInputValue);
-    elInput.value = ""
+    elInput.value = "";
     elList.appendChild(elItem);
     console.log(`Maxsulot: = ${foodElement}`);
     elItem.textContent = `Maxsulot nomi = ${foodElement}`;
      return (foodElement);
-     if (foodElement > 0) {
-    elItem.textContent = "son kiritmang"
-     }
+
   };
 });
+
 
