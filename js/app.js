8a75b5ab45e20202723ddf1a0f057b0fbd74500e
Hamidulloh07 <hsiddiqxojayev2005@gmail.com>
2022-08-09 17:26:03 +0600

    add a lot files

---
+++ js/app.js
@@ -0,0 +1,36 @@
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".form-control");
+let elList = document.querySelector(".list");
+
+
+
+elForm.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+
+ elInputValue = (elInput.value);
+
+  for (foodElement of elInputValue) {
+
+    foodElement = []
+
+    let elItem = document.createElement("li");
+    
+    foodElement.push(elInputValue);
+    elInput.value = ""
+    elList.appendChild(elItem);
+    console.log(foodElement);
+    elItem.textContent = foodElement;
+     return (foodElement);
+     if (foodElement > 0) {
+      elItem.textContent = "son kiritmang"
+     }
+
+  
+     
+  };
+
+
+      
+
+});
+
